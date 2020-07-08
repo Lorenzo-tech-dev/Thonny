21b58c95b692f1d656a7814baa94c4e65862f25f
Lorenzo-tech-dev <68027800+Lorenzo-tech-dev@users.noreply.github.com>
2020-07-08 15:54:40 -0300

    Update PROJETO SUPERMERCADO INTELIÊNTE.py

--- PROJETO SUPERMERCADO INTELIÊNTE.py
+++ PROJETO SUPERMERCADO INTELIÊNTE.py
@@ -1,11 +1,16 @@
 '''08/07/2020 13:24 Criar uma matriz e as chaves serão os produtos de uma prateleira(5 linhas) com (5 produtos).
-Criar uma variável de pesquisa e a pessoa pesquisa sabonete, para saber se existe o sabonete e em qual corredor ele está!'''
+Criar uma variável de pesquisa e a pessoa pesquisa sabonete, para saber se existe o sabonete e em qual corredor ele está!!'''
 
-lista = [
-     ["geleia", "leite", "ovo", "alface"],
+lista = {
+    'corrdor1':["geleia", "leite", "ovo", "alface"],
+    'corrdor2':["picles", "chantili", "requeijao", "tomate"],
+    'corrdor3':["molho de tomate", "iogurte", "carne", "banana"],
+}
+ '''   
+lista = ('corrdor1': ["geleia", "leite", "ovo", "alface"],
     ["picles", "chantili", "requeijao", "tomate"],
     ["molho de tomate", "iogurte", "carne", "banana"]
-]
+)
 a = lista[0]
 '''
 def search (lista, valor):
@@ -14,7 +19,9 @@ def search (lista, valor):
             for x in lista
                 if valor in x
     ]
+
+    
 a = str(input("Digite o alimento desejado: "))
 print(search(lista, a))
-'''
+
 print(a)
