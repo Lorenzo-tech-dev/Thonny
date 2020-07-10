12f83cf5a2dacdf55968f4f867e6a810db662a3a
Lorenzo-tech-dev <68027800+Lorenzo-tech-dev@users.noreply.github.com>
2020-07-09 21:42:32 -0300

    Create DSA - Python-Cap 3-Exercícios 5 REVER.py
    line 5 to 8 is the correct program.. nowarday is hard for me understand why i need put in line 5 and 8. I put some comments to study tomorrow

---
+++ DSA - Python-Cap 3-Exercícios 5 REVER.py
@@ -0,0 +1,28 @@
+# Exercício 5 - Crie uma variável chamada temperatura
+#e atribua o valor 40. Enquanto temperatura for maior 
+# que 35, imprima as temperaturas na tela
+
+temperatura = 40  
+while temperatura > 35: 
+    print(temperatura)
+    temperatura = temperatura - 1
+#me explica a ligação que tem da linha 5 e livra 8!
+#eu quero entender tudo
+#que ela significa e o que acontece se mudar alguma coisa nela.
+
+'''
+temperatura = 40
+for c in range(35,41):
+    print(c)
+'''
+
+'''
+temperatura = 40
+while temperatura > 35:
+    if t == 40:
+        break
+    
+    print(t)
+    
+    t += 1
+'''
