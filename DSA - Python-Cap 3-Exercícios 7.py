a7fbf18bc32feaf0d17b3dc082698c5aeae1da6f
Lorenzo-tech-dev <68027800+Lorenzo-tech-dev@users.noreply.github.com>
2020-07-09 21:46:38 -0300

    Update DSA - Python-Cap 3-Exercícios 7.py

--- DSA - Python-Cap 3-Exercícios 7.py
+++ DSA - Python-Cap 3-Exercícios 7.py
@@ -1,11 +1,6 @@
 # Exercício 7 - Crie uma lista vazia e uma variável com valor 4. Enquanto o valor da variável for menor ou igual a 20, 
 # adicione à lista, apenas os valores pares e imprima a lista
 
-lista = []
-contador = 4
-while contador<=20:
-    if contador % contador == 1:
-        print(contador)
-    contador += 1
     
-    #####ERRO#######
+
+       
